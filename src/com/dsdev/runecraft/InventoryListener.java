524cbb9b77fded809babfb462eb629d4d63f6b30
Nathan2055 <nathan2055@private.fake>
2013-07-23 12:25:44 -0400

    Cleanup mess from package refactor

--- src/com/dsdev/runecraft/InventoryListener.java
+++ src/com/dsdev/runecraft/InventoryListener.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.Greenlock28.RuneCraft;
+package com.dsdev.runecraft;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/com/dsdev/runecraft/RuneCraft.java
+++ src/com/dsdev/runecraft/RuneCraft.java
@@ -4,7 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.dsdev.runecraft.EventListener;
 
 public class RuneCraft extends JavaPlugin
 {
